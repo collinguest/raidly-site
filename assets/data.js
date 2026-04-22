864eee47849cad1ee3ffe390476227094b9526ab
collinguest <42694491+collinguest@users.noreply.github.com>
2026-04-22 19:11:54 +0100

    Create assets/data.js

---
+++ assets/data.js
@@ -0,0 +1,7 @@
+const SERVER_DATA = [
+  { name: "Gaming Hub", installed: true },
+  { name: "Streamer Lounge", installed: false },
+  { name: "Community Server", installed: true },
+  { name: "Esports Network", installed: true },
+  { name: "Chill Zone", installed: false }
+];
